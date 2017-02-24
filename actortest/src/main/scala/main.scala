16abe51764c7dcc37dcc7af06799ddd6296fc394
Greg Bull <gregorycb@outlook.com>
2017-02-24 01:35:05 +0000

    edited

--- actortest/src/main/scala/main.scala
+++ actortest/src/main/scala/main.scala
@@ -1,4 +1,8 @@
 import akka.actor._
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
+import scala.concurrent.{Await, ExecutionContext, Future}
 
 case class Msg(str:String) 
 
@@ -9,13 +13,30 @@ class Printer extends Actor {
 	}
 }
 
+class Adder extends Actor {
+	def receive = {
+		case i:Int =>
+			Thread.sleep(2000)
+			sender ! (i+1)
+	}
+}
+
 object AkkaActors {
 	def main(args: Array[String]){
 
 		val system = ActorSystem("SimpleSystem")
+		
+		// Hello World example
 		val printer = system.actorOf(Props[Printer], name = "Printer")
 		
 		// use ! (bang) to send the message to the actor
 		printer ! Msg("Hello world")
+		
+		val adder = system.actorOf(Props[Adder], name = "Adder")
+		implicit val timeout = Timeout(5 seconds)
+		val future = adder ? 1
+		val result = Await.result(future, timeout.duration)
+		println(result)
+		system shutdown
 	}
 }
